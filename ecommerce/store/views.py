ed5d8f6b0621b9cef9779b0e4abf5611b1e366f3
Joy Ahmed <dev.joyahmed@gmail.com>
2023-09-15 18:25:29 +0600

    finished dynamic render in cart and checkout

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -9,10 +9,24 @@ def store(request):
 
 
 def cart(request):
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = [] #log out state
+        order = {'get_cart_items': 0, 'get_cart_total': 0} #log out state
+    context = {'items': items, 'order': order}
     return render(request, 'store/cart.html', context)
 
 
 def checkout(request):
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = [] #log out state
+        order = {'get_cart_items': 0, 'get_cart_total': 0} #log out state
+    context = {'items': items, 'order': order}
     return render(request, 'store/checkout.html', context)
